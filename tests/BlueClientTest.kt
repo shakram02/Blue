55880845d524735c094fe956af8f1e8a5940d5b8
shakram02 <ahmedhamdyau@gmail.com>
2018-03-05 09:48:32 +0200

    create tests directory

--- tests/BlueClientTest.kt
+++ tests/BlueClientTest.kt
@@ -25,27 +25,40 @@ class BlueClientTest {
 
     @org.junit.Test
     fun aConnect() {
+        waitNetworkOperation()  // Wait for Server to start
+
         val socket = BlueClient()
         var hasConnected = false
         socket.onConnected += { hasConnected = true }
         socket.connect("localhost", 60001)
 
-        Thread.sleep(200)
+        waitNetworkOperation()
         Assert.assertTrue(hasConnected)
         socket.close()
     }
 
     @Test
     fun bRead() {
+        waitNetworkOperation()
+
         val socket = BlueClient()
         var hasConnected = false
         var received = ""
         socket.onConnected += { hasConnected = true }
         socket.onReceived += { b -> received = String(b) }
+
         socket.connect("localhost", 60001)
+        waitNetworkOperation()
+
         socket.send("Hello world".toByteArray())
-        Thread.sleep(200)
+        waitNetworkOperation()
+
+        socket.close()
         Assert.assertTrue(hasConnected)
         Assert.assertTrue(received.isNotEmpty())
+    }
+
+    private fun waitNetworkOperation(){
+        Thread.sleep(20)
     }
 }
