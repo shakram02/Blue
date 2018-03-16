d0762152e2c9ec0e5a2683320314884775c5e495
shakram02 <ahmedhamdyau@gmail.com>
2018-03-16 13:55:24 +0200

    add disconnected event, refactor handlers, fix close bug

--- src/main/kotlin/shakram02/blue/BlueClient.kt
+++ src/main/kotlin/shakram02/blue/BlueClient.kt
@@ -11,37 +11,41 @@ import java.nio.channels.AsynchronousSocketChannel
 
 
 class BlueClient : Closeable {
-    private val readHandler = HandlerUtils.toHandler { byteCount: Int, _: Nothing? -> finishRead(byteCount) }
-    private val writeHandler = HandlerUtils.toHandler { x: Int?, _: Nothing? -> onSent(x!!) }
-    private val onConnectHandler = HandlerUtils.toHandler({ _: Void?, _: Nothing? ->
-        run { onConnected(Unit); readNext() }
-    })
+    private val readHandler = HandlerUtils.toHandler { byteCount: Int, bytes: ByteArray? ->
+        onReceived(bytes!!);
+        finishRead(byteCount)
+    }
+
+    private val writeHandler = HandlerUtils.toHandler { _: Int?, bytes: ByteArray? -> onSent(bytes!!) }
+    private val onConnectHandler = HandlerUtils.toHandler({ _: Void?, _: Nothing? -> onConnected(Unit) })
 
     private val rb = allocateDirect(1024)
     private val channel: AsynchronousSocketChannel = AsynchronousSocketChannel.open()
 
     val onReceived = Event<ByteArray>()
-    val onSent = Event<Int>()
+    val onSent = Event<ByteArray>()
     val onConnected = Event<Unit>()
+    val onDisconnected = Event<Unit>()
 
     fun connect(ip: String, port: Int) {
         channel.connect(InetSocketAddress(ip, port), null, onConnectHandler)
+        channel.read(rb, null, readHandler)
     }
 
     private fun finishRead(byteCount: Int) {
-        if (byteCount == -1) return
-        onReceived(HandlerUtils.read(rb).toByteArray())
-        readNext()
+        if (byteCount == -1) {
+            onDisconnected(Unit)
+            return
+        }
+
+        val bytes = HandlerUtils.read(rb).toByteArray()
+        onReceived(bytes)
+        channel.read(rb, bytes, readHandler)
     }
 
     fun send(bytes: ByteArray) {
         val wb = ByteBuffer.wrap(bytes)
-        // Replace null with attachment object
-        channel.write(wb, null, writeHandler)
-    }
-
-    private fun readNext() {
-        channel.read(rb, null, readHandler)
+        channel.write(wb, bytes, writeHandler)
     }
 
     @Throws(IOException::class)
@@ -50,7 +54,7 @@ class BlueClient : Closeable {
 
         try {
             this.channel.shutdownOutput()
-            this.channel.shutdownOutput()
+            this.channel.shutdownInput()
             this.channel.close()
         } catch (e: AsynchronousCloseException) {
             e.printStackTrace()
